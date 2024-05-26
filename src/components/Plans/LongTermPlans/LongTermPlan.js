ad8265a80ef2017035ef2f48e3d0504ef3e96e97
Yi Long <280132947@qq.com>
2024-05-26 14:16:36 +1000

    Get rid of the plan fetched from database and replace it with redux plan
    Show the redux plan after posting the data

--- src/components/Plans/LongTermPlans/LongTermPlan.js
+++ src/components/Plans/LongTermPlans/LongTermPlan.js
@@ -1,57 +1,17 @@
-import React, { useCallback, useState, useEffect, useContext } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 /* ========== import React components ========== */
 import NewLongTermPlan from '../NewPlan/NewLongTermPlan';
-import ShortTermPlans from '../ShortTermPlans/ShortTermPlans'
-import AuthContext from '../../../store/auth-context';
 
 /* ========== import css ========== */
 import classes from './LongTermPlan.module.css';
 
 const LongTermPlan = () => {
-    // plan is empty object
-    const [planId, setPlanId] = useState("");
-    const [plan, setPlan] = useState({});
-    const [isFetch, setIsFetch] = useState(false);
-
-    // Object for interacting with database endpoint
-    const authCtx = useContext(AuthContext);
-    const instance = authCtx.firebase;
-
-    // get data from database function
-    const fetchPlansHandler = useCallback(async () => {
-        let plan_id = "";
-        let _plan = {};
-
-        if(!isFetch){
-            const response = await instance.get('/long_term_plans/active_plans.json');
-            const data = response.data;
-            if(data != null){
-                for (let index in data) {
-                    plan_id = index;
-                    _plan = data[index];
-                }
-            }
-            setIsFetch(true);
-            setPlanId(planId => plan_id);
-            setPlan(plan => _plan);
-        }
-    }, [isFetch, instance])
+    const plan = useSelector((state) => state.activePlan);
 
     const archivePlan = () => {
-        // Migrate the plan from active_plans to history_plans
-        console.log("Updating the database...");
-        instance.post(`/long_term_plans/history_plans.json`, plan);
-
-        // Delete the plan in active_plans
-        console.log("Updating the database...");
-        instance.delete(`/long_term_plans/active_plans/${planId}.json`)
-        .then(response => {
-            if(response.status === 200) {
-                // Refresh the page
-                window.location.reload();
-            }
-        })
+        // TODO
     }
 
     const showLongTermPlan = (
@@ -61,27 +21,16 @@ const LongTermPlan = () => {
                     <h3>Marathon</h3>
                     <button onClick={archivePlan}>Done</button>
                 </div>
-                <h5>{plan['title']}</h5>
-                <div>{plan['description']}</div>
+                <h5>{plan.title}</h5>
+                <div>{plan.description}</div>
             </section>
-            <ShortTermPlans long_term_plan_id={planId} long_term_plan={plan}/>
         </div>
     )
 
-    // get the data from database as soon as user visit the home page
-    useEffect(() => {
-        fetchPlansHandler();
-    }, [fetchPlansHandler]);
-
     return (
         <React.Fragment>
-            {Object.keys(plan).length === 0 &&
-                <NewLongTermPlan />
-            }
-
-            {Object.keys(plan).length > 0 &&
-                showLongTermPlan
-            }
+            {plan.title === "" && <NewLongTermPlan />}
+            {plan.title !== "" && showLongTermPlan}
         </React.Fragment>
     )
 }
