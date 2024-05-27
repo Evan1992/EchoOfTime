95bb58166d3b251c68b50b5019be0e8dde60c668
Yi Long <280132947@qq.com>
2024-05-27 11:33:21 -0700

    Display the daily plans

--- src/components/Plans/DailyPlans/DailyPlan.js
+++ src/components/Plans/DailyPlans/DailyPlan.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-const DailyPlan = () => {
+/* ========== import other libraries ========== */
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+/* ========== import css ========== */
+import classes from './DailyPlan.module.css';
+
+const DailyPlan = (props) => {
     return (
         <React.Fragment>
+            <Row>
+                <Col xs={1} />
+                <Col xs='auto'>
+                    <div className={classes.expand_collapse} onClick={props.childrenToggleHandler} >
+                        {/* Ternary expression: render the icon conditionally based on the state show_children using ternary operator */}
+                        {
+                            // do not show the expand/shrink icon if no children
+                            (typeof props.plan.children !== 'undefined') &&
+                            (
+                                props.show_children ?
+                                <img className={classes.expand_collapse_img} src='https://img.icons8.com/ios-filled/50/000000/collapse-arrow.png'  alt='collapse' /> :
+                                <img className={classes.expand_collapse_img} src='https://img.icons8.com/material-rounded/24/000000/more-than.png' alt='expand' />
+                            )
+                        }
+                    </div>
+                </Col>
 
+                <Col xs={{ span: 4}} style={{display:'flex', justifyContent:'left'}}>
+                    <div>{props.plan.title || 'No title'}</div>
+                </Col>
+            </Row>
         </React.Fragment>
     )
 }
--- src/components/Plans/DailyPlans/DailyPlans.js
+++ src/components/Plans/DailyPlans/DailyPlans.js
@@ -1,13 +1,48 @@
 /* ========== import React and React hooks ========== */
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+/* ========== import React components ========== */
 import NewDailyPlan from './NewDailyPlan';
+import DailyPlan from './DailyPlan';
+
+/* ========== import other libraries ========== */
+import Container from 'react-bootstrap/Container';
+
+/* ========== import css ========== */
+import classes from './DailyPlans.module.css';
 
 const DailyPlans = () => {
+    const plan = useSelector((state) => state.activePlan);
+
     return (
         <React.Fragment>
-            <NewDailyPlan />
+            {/* Component for all active plans */}
+            <div className={classes.plans}>
+                <Container fluid className={classes.container}>
+                    {
+                        plan.short_term_plan.daily_plans &&
+                        plan.short_term_plan.daily_plans.map((dailyPlan) => {
+                            return <DailyPlan
+                                        key={dailyPlan.id}
+                                        plan={dailyPlan}
+                                    />
+                        })
+                    }
+                    <NewDailyPlan />
+                </Container>
+            </div>
         </React.Fragment>
     )
 }
 
 export default DailyPlans
+
+
+
+/* ========== Learning ========== */
+/* Rendering list */
+// Reference: https://react.dev/learn/rendering-lists
+// When displaying multiple similar components from a collection of data,
+// we can use the JavaScript array methods filter() and map() to manipulate
+// an array of data
