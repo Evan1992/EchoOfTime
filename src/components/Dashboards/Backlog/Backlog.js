da67d43c0bb2acfb8bc3ad6e8c0e2ac10eeb0ccc
Yi Long <280132947@qq.com>
2025-05-25 15:30:23 -0700

    Initial content for Backlog page

--- src/components/Dashboards/Backlog/Backlog.js
+++ src/components/Dashboards/Backlog/Backlog.js
@@ -1,21 +1,87 @@
-import React from 'react';
+/* ========== import React and React hooks ========== */
+import React, { useState, useEffect, useContext } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+/* ========== import React components ========== */
+import NewDailyPlan from '../../Plans/DailyPlans/NewDailyPlan';
+import DailyPlan from '../../Plans/DailyPlans/DailyPlan';
+import AuthContext from '../../../store/auth-context';
 
 /* ========== import other libraries ========== */
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/esm/Container';
+import Container from 'react-bootstrap/Container';
+import { isTomorrow } from '../../../utilities';
+import { sendPlanData } from '../../../store/slices/active-plan-actions';
+
+/* ========== import css ========== */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import classes from './Backlog.module.css';
+
 
 const Backlog = () => {
+    const authCtx = useContext(AuthContext);
+    const [planDeleted, setPlanDeleted] = useState(false);
+    // Only one timer for a task can be active at a time
+    const [isTimerActive, setIsTimerActive] = useState(false);
+    const [timerHolder, setTimerHolder] = useState(null);
+    const [highlight, setHighlight] = useState(null);
+    const dispatch = useDispatch();
+    const plan = useSelector((state) => state.activePlan);
+
+    const tomorrowPlans = []
+    if(plan.short_term_plan.daily_plans !== undefined) {
+        for(const daily_plan of plan.short_term_plan.daily_plans) {
+            if(isTomorrow(daily_plan.date)) {
+                tomorrowPlans.push(daily_plan)
+            }
+        }
+    }
+
+    useEffect(() => {
+        if(planDeleted === true) {
+            // No need to do sendDailyPlanData as sendPlanData will update the parent object
+            // dispatch(sendDailyPlanData(authCtx, plan))
+            dispatch(sendPlanData(authCtx, plan))
+            setPlanDeleted(false);
+        }
+    }, [dispatch, authCtx, plan, planDeleted])
+
     return (
         <React.Fragment>
-            <Container fluid>
-                <Row>
-                    <Col xs={1} />
-                    <Col>
-                        <h4>Backlog</h4>
-                    </Col>
-                </Row>
-            </Container>
+            {/* Component for all active plans */}
+            <div className={classes.plans}>
+                <Container>
+                    {
+                        plan.short_term_plan.daily_plans &&
+                        plan.short_term_plan.daily_plans.map((dailyPlan, index) => {
+                            let show_children = false;
+                            if(dailyPlan.has_children) {
+                                if(index+1 < plan.short_term_plan.daily_plans.length && plan.short_term_plan.daily_plans[index+1].show_plan) {
+                                    show_children = true;
+                                }
+                            }
+                            if(dailyPlan.show_plan) {
+                                return <DailyPlan
+                                    key={dailyPlan.id}
+                                    id={dailyPlan.id}
+                                    plan={plan}
+                                    daily_plan={dailyPlan}
+                                    rank={dailyPlan.rank}
+                                    show_children={show_children}
+                                    set_plan_deleted={setPlanDeleted}
+                                    isTimerActive={isTimerActive}
+                                    setIsTimerActive={setIsTimerActive}
+                                    timerHolder={timerHolder}
+                                    setTimerHolder={setTimerHolder}
+                                    highlight={highlight}
+                                    setHighlight={setHighlight}
+                                />
+                            }
+                            return <div key={dailyPlan.id} />
+                        })
+                    }
+                    <NewDailyPlan />
+                </Container>
+            </div>
         </React.Fragment>
     )
 }
