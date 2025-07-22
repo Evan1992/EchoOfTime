95b4a10287018347432dca5a6a87188a6b5b80b8
Yi Long <280132947@qq.com>
2025-07-21 20:45:36 -0700

    Fetch the data as soon as user visit Sprint page

--- src/components/Dashboards/Sprint/Sprint.js
+++ src/components/Dashboards/Sprint/Sprint.js
@@ -1,13 +1,31 @@
 /* ========== import React and React hooks ========== */
 import React from 'react';
+import { useEffect, useContext } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 /* ========== import React components ========== */
 import DailyPlans from '../../Plans/DailyPlans/DailyPlans';
+import AuthContext from '../../../store/auth-context';
+
+/* ========== import other libraries ========== */
+import { fetchPlanData } from '../../../store/slices/active-plan-actions';
 
 /* ========== import css ========== */
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Sprint = () => {
+    const authCtx = useContext(AuthContext);
+    const plan = useSelector((state) => state.activePlan);
+    const dispatch = useDispatch();
+
+    // Get the data from database as soon as user visit Sprint page
+    useEffect(() => {
+        // Check if the plan is already fetched
+        if (plan.title === "") {
+            dispatch(fetchPlanData(authCtx));
+        }
+    }, [plan, authCtx, dispatch])
+
     return (
         <React.Fragment>
             {/* Separation between Navigation and DailyPlans */}
