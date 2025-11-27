4e1097d1e9bac982a77d6cf6e02bb90910f9e31d
Yi Long <280132947@qq.com>
2025-11-26 21:56:01 -0800

    Modify the comment in active-plan-slice.js

--- src/store/slices/active-plan-slice.js
+++ src/store/slices/active-plan-slice.js
@@ -136,7 +136,7 @@ const addDailyPlanToList = (state, action, dailyPlans)  => {
                 }
             }
         }
-        // Add daily plan to state.today.today_plans if the plan is added from Today page
+        // Add daily plan to state.today.today_plans if the plan is added eithr from Today page or through Todo Everyday
         if (action.payload.daily_plan.date !== "" && isToday(action.payload.daily_plan.date)) {
             if (state.today.today_plans !== undefined) {
                 state.today.today_plans.push(action.payload.daily_plan)
